dd9d68e7aacf2c078c35899ad8eb41970655fb80
rajeev <rajeevamazon1@yahoo.in>
2020-12-05 22:46:10 +0530

    week 4; regular exercises complete

---
+++ it/week4/Q3/Q3/ShoppingCart.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ShoppingCart.aspx.cs" Inherits="Q3.ShoppingCart" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 402px">
+    <form id="form1" runat="server">
+        <div style="height: 375px">
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Name: "></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Shopping Cart: "></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Continue Shopping" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ it/week4/Q3/Q3/ShoppingCart.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Q3
+{
+    public partial class ShoppingCart : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                Label1.Text = "Welcome, New User !";
+            }
+
+            HttpCookie cookie = Request.Cookies["shop_cart"];
+            if(cookie != null)
+            {
+                TextBox2.Text = (string)cookie["items"];
+                Label1.Text = "Welcome back, " + (string)cookie["name"] + " !";
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("shop_cart");
+            cookie["items"] = TextBox2.Text;
+            cookie["name"] = TextBox1.Text;
+            cookie.Expires = DateTime.Now.AddMinutes(60);
+            Response.Cookies.Add(cookie);
+        }
+    }
+}
