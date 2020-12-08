6ddbbb029f88818a3a39449b1f43e7b6ff547330
rajeev <rajeevamazon1@yahoo.in>
2020-12-09 01:29:05 +0530

    week8; q3

---
+++ it/week8/q3/Q3/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Q3
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           	string file = Path.Combine(Request.PhysicalApplicationPath, @"App_Data\Cars.xml");
+            XElement ele1 = new XElement("Detail", 
+                new XElement("Name", "I20"), 
+                new XElement("Make", "Hyundai"), 
+                new XElement("Price", "70000")
+            );
+
+            XElement ele2 = new XElement("Detail",
+                new XElement("Name", "I20"),
+                new XElement("Make", "Hyundai"),
+                new XElement("Price", "70000")
+            );
+
+            XElement ele = new XElement("Car",
+                from el in ele1.Elements()
+                select el
+            );
+
+            ele.Add(ele2);
+
+            XDocument doc = new XDocument(ele);
+
+            doc.Save(file);
+        }
+    }
+}
